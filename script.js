0813072e9612489c7e4f58dfcbbb3c1599e32564
manrajsingh248 <manraj.singh248@gmail.com>
2022-12-04 14:57:04 -0500

    Used switch statements to perform specific actions based on the values of the innerText

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-/* Declaring/assigning 2 variables to select the element 
-that matches it's specific CSS selector */
-
 let textbox = document.getElementById('textbox');
+
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map(button => {
+buttons.map( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
+            case 'C':
+                textbox.innerText = '';
+                break;
+            case 'DEL':
+                if(textbox.innerText) {
+                    textbox.innerText = textbox.innerText.slice(0, -1);
+                    break; 
+                }
+            
+            case '=':
+                textbox.innerText = eval(textbox.innerText);
+                break; 
+
             
             default:
-                textbox.innerText += e.target.innerText;
+              textbox.innerText += e.target.innerText;
+
+
+
         }
     });
 });
